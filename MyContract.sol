1fda5b863a7b0359305f6e8d3f99ccd21888e937
Shuga8 <lotocharles8@gmail.com>
2024-04-09 13:05:12 +0100

    continue

--- MyContract.sol
+++ MyContract.sol
@@ -14,21 +14,31 @@ contract Ownable{
     }
 }
 
-contract MyContract is Ownable {
-
-    // Inheritances
-    // Factories
-    // Interaction
-
+contract SecretVault{
     string secret;
 
     constructor(string memory _secret) {
         secret = _secret;
+    }
+
+    function getSecret() public view returns(string memory){
+        return secret;
+    }
+}
+
+contract MyContract is Ownable {
+
+    address secretVault;
+
+    constructor(string memory _secret) {
+        SecretVault _secretVault = new SecretVault(_secret);
+        secretVault = address(_secretVault);
         super;
+
     }
 
     function getSecret() public view onlyOwner returns(string memory){
-        return secret;
+        return SecretVault(secretVault).getSecret();
     }
 
 
